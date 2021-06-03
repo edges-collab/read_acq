8f2885a02994e8334a58a2f458614ceec4115bce
Steven Murray <steven.g.murray@asu.edu>
2021-06-03 15:04:13 -0700

    use pypi repo for edges-io

--- setup.py
+++ setup.py
@@ -58,7 +58,7 @@ setup(
             "edges-io",
         ],
         "h5": [
-            "edges-io @ git+git://github.com/edges-collab/edges-io.git",
+            "edges-io",
         ],
     },
     ext_modules=[
