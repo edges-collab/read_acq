ff374226474ff77eed90a0da5f73e9eb15669184
Steven Murray <steven.g.murray@asu.edu>
2024-06-04 16:09:08 +0200

    maint: modernize version inference

--- src/read_acq/__init__.py
+++ src/read_acq/__init__.py
@@ -1,16 +1,16 @@
 """Package providing functionality for reading .ACQ format files."""
 
-import contextlib
-
 __all__ = ["decode_file", "encode", "read_acq_to_gsdata", "write_gsdata_to_acq"]
-from pkg_resources import DistributionNotFound, get_distribution
+
+from importlib.metadata import PackageNotFoundError, version
 
 from .gsdata import read_acq_to_gsdata, write_gsdata_to_acq
 from .read_acq import decode_file, encode
 
-with contextlib.suppress(DistributionNotFound):
-    __version__ = get_distribution(__name__).version
-
-del get_distribution
-del DistributionNotFound
-del contextlib
+try:
+    __version__ = version(__name__)
+except PackageNotFoundError:
+    # package is not installed
+    __version__ = "unknown"
+finally:
+    del version, PackageNotFoundError
